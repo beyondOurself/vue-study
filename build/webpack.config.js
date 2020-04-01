53a3d3c2f5fec130a9005a9843ec60feb5ba550a
longlong <562172151@qq.com>
2020-04-02 00:18:02 +0800

    init

---
+++ build/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+	entry: {
+		bundle: "./src/index.js"
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, '../dist')
+	},
+	devtool: 'eval-source-map',
+	devServer: {
+		contentBase: './dist',
+		hot: true
+	},
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: [
+				'style-loader',
+				'css-loader'
+			]
+		}]
+	},
+	plugins: [
+		//替换 html的插件
+		new HtmlWebpackPlugin({
+			title: '',
+			template: path.resolve(__dirname, '../index.html'),
+			inject: 'head' // body,head
+		}),
+	]
+	mode: "development"
+}
