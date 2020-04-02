11fe822f00883401a8f6d0ba3f0ba0ce5fadcafc
15099836861 <562172151@qq.com>
2020-04-02 17:38:23 +0800

    修改 路由路径

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
       <!-- 使用 router-link 组件来导航. -->
       <!-- 通过传入 `to` 属性指定链接. -->
       <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
-      <router-link to="/one">Go to one</router-link>
-      <router-link to="/two">Go to two</router-link>
+      <router-link to="/one/55">Go to one</router-link>
+      <router-link to="/two/long/post/666">Go to two</router-link>
       <router-link to="/three/one">响应路由参数的变化one</router-link>
       <router-link to="/three/two">响应路由参数的变化two</router-link>
     </p>
