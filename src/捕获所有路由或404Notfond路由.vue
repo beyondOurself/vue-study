7b076fe52674772080ec95c5447ca9722b73b582
15099836861 <562172151@qq.com>
2020-04-03 09:01:45 +0800

    新增 捕获所有路路由或404notfound路由

---
+++ src/捕获所有路由或404Notfond路由.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>捕获所有路由或404Notfond路由</div>
+</template>
+<script>
+	export default{
+		
+		
+		mounted() {
+			console.log(">>>>>"+this.$route.params.username);
+			console.log(">>>>>"+this.$route.params.post_id);
+			console.log(">>>>>"+this.$route.params.pathMatch);
+		}
+	}
+</script>
